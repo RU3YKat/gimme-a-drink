ac288b7f6a04b945f15e22d2af8673ea8ab7fa8b
insomniKat <ru3ykat@gmail.com>
2021-10-03 15:47:38 -0400

    new yt player trial

--- assets/js/player.js
+++ assets/js/player.js
@@ -4,18 +4,25 @@ var widget1         = SC.Widget(iframeElement);
 var widget2         = SC.Widget(iframeElementID);
 // widget1 === widget2
 
-    (function(){
-        var widgetIframe = document.getElementById('sc-widget'),
-            widget       = SC.Widget(widgetIframe),
-            newSoundUrl = 'https://api.soundcloud.com/tracks/13692671';
+  SC.initialize({
+    client_id: 'YOUR_CLIENT_ID',
+    redirect_uri: 'https://example.com/callback'
+  });
 
-        widget.bind(SC.Widget.Events.READY, function() {
-        // load new widget
-        widget.bind(SC.Widget.Events.FINISH, function() {
-            widget.load(newSoundUrl, {
-            show_artwork: false
-            });
-        });
-        });
 
-    }());
+
+    // (function(){
+    //     var widgetIframe = document.getElementById('sc-widget'),
+    //         widget       = SC.Widget(widgetIframe),
+    //         newSoundUrl = 'https://api.soundcloud.com/tracks/13692671';
+
+    //     widget.bind(SC.Widget.Events.READY, function() {
+    //     // load new widget
+    //     widget.bind(SC.Widget.Events.FINISH, function() {
+    //         widget.load(newSoundUrl, {
+    //         show_artwork: false
+    //         });
+    //     });
+    //     });
+
+    // }());
