0abe127a16e2242508304ffc5cf77254d6bc836c
insomniKat <ru3ykat@gmail.com>
2021-10-03 12:44:14 -0400

    update widget format to encompass html and jS

---
+++ assets/js/player.js
@@ -0,0 +1,21 @@
+var iframeElement   = document.querySelector('iframe');
+var iframeElementID = iframeElement.id;
+var widget1         = SC.Widget(iframeElement);
+var widget2         = SC.Widget(iframeElementID);
+// widget1 === widget2
+
+    (function(){
+        var widgetIframe = document.getElementById('sc-widget'),
+            widget       = SC.Widget(widgetIframe),
+            newSoundUrl = 'https://api.soundcloud.com/tracks/13692671';
+
+        widget.bind(SC.Widget.Events.READY, function() {
+        // load new widget
+        widget.bind(SC.Widget.Events.FINISH, function() {
+            widget.load(newSoundUrl, {
+            show_artwork: false
+            });
+        });
+        });
+
+    }());
